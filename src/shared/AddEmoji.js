22f7210d20172b7589084ef5dff55fbfac6e751b
Babyrusa <imdatingfood@gmail.com>
2020-02-09 21:29:45 -0500

    well tryna fix cbe emoji error

--- src/shared/AddEmoji.js
+++ src/shared/AddEmoji.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import { Smile } from "react-feather";
-import { Picker } from "emoji-mart";
+import { Picker, Emoji } from "emoji-mart";
 import "emoji-mart/css/emoji-mart.css";
+import data from 'emoji-mart/data/emojione.json'
 
 export default class AddEmoji extends Component {
   constructor(props) {
     super(props);
+    const sheetUrl = Emoji.defaultProps.backgroundImageFn('emojione', 64)
+    const oReq = new XMLHttpRequest();
+    oReq.open("GET", sheetUrl);
+    oReq.send();
     this.state = {
       showEmojiPicker: false
     };
@@ -28,7 +33,8 @@ export default class AddEmoji extends Component {
           <Smile size={this.props.size ?  this.props.size : '24'}/>
         </button>
         {this.state.showEmojiPicker && (
-          <Picker set="emojione" onSelect={this.props.addEmoji} />
+          <Picker set="emojione" sheetSize="32" onSelect={this.props.addEmoji} 
+          title='Pick your emoji…' emoji='point_up' color="#ff66b6"/>
         )}
       </React.Fragment>
     );
